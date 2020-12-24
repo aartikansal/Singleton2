87464b418ece9e6a219f894243fd528952254878
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-12-23 22:41:56 -0500

    Add files via upload

--- Earth.java
+++ Earth.java
@@ -1,2 +1,18 @@
-public class Earth {
+
+public class Earth implements Planet {
+    private static volatile Earth instance = null;
+
+    private Earth() {
+    }
+
+    public static Earth getInstance() {
+        if (instance == null) {
+            synchronized (Earth.class) {
+                if (instance == null) {
+                    instance = new Earth();
+                }
+            }
+        }
+        return instance;
+    }
 }
