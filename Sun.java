a2396907b57244102bdb07222bfee9c97d1e787f
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-12-23 22:40:46 -0500

    Add files via upload

--- Sun.java
+++ Sun.java
@@ -1,2 +1,18 @@
-public class Sun {
+public class Sun implements Planet {
+    private static volatile Sun instance = null;
+
+    private Sun() {
+    }
+
+    public static Sun getInstance() {
+        if (instance == null) {
+            synchronized (Sun.class) {
+                if (instance == null) {
+                    instance = new Sun();
+                }
+            }
+        }
+        return instance;
+    }
+
 }
