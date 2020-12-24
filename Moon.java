a0309f0d18f6db0fc63539bf46e20df3e6d4b0a5
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-12-23 22:39:42 -0500

    Add files via upload

--- Moon.java
+++ Moon.java
@@ -1,2 +1,18 @@
-public class Moon {
+
+public class Moon implements Planet {
+    private static volatile Moon instance = null;
+
+    private Moon() {
+    }
+
+    public static Moon getInstance() {
+        if (instance == null) {
+            synchronized (Moon.class) {
+                if (instance == null) {
+                    instance = new Moon();
+                }
+            }
+        }
+        return instance;
+    }
 }
