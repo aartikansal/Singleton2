9f77f04a0d275e7adfdc1e87a2ed2ebdd0562580
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-12-23 22:45:02 -0500

    Add files via upload

--- RE_Singleton.java
+++ RE_Singleton.java
@@ -1,2 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+Reinforce the singleton pattern
+*/
 public class RE_Singleton {
-}
+        public static void main(String[] args) {
+
+        }
+
+        public static Planet thePlanet;
+
+        static {
+            try {
+                readKeyFromConsoleAndInitPlanet();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public static void readKeyFromConsoleAndInitPlanet() throws IOException {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String consoleInput = reader.readLine();
+            switch (consoleInput) {
+                case Planet.EARTH :
+                    thePlanet = Earth.getInstance();
+                    break;
+                case Planet.MOON :
+                    thePlanet = Moon.getInstance();
+                    break;
+                case Planet.SUN :
+                    thePlanet = Sun.getInstance();
+                    break;
+                default:
+                    thePlanet = null;
+                    break;
+            }
+        }
+    }
+
